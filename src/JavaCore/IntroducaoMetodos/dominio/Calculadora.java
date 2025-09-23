e100755580a76805f51ffe6752094b827f03194f
Pedro brunet <joaopedrobrunet@hotmail.com>
2025-09-23 20:36:29 -0300

    atualização

--- src/JavaCore/IntroducaoMetodos/dominio/Calculadora.java
+++ src/JavaCore/IntroducaoMetodos/dominio/Calculadora.java
@@ -39,4 +39,12 @@ public class Calculadora {
         System.out.println("Num1: " + num1);
         System.out.println("Num2: " + num2);
     }
+
+    public void somaArray (int[] numeros){
+        int soma = 0;
+        for (int num: numeros){
+            soma += num;
+            System.out.println(soma);
+        }
+    }
 }
