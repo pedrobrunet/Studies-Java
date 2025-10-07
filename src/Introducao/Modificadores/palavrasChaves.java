ded2290d5cf1ef5cdea1d9dbc55f5ebe2627c2f0
Pedro brunet <joaopedrobrunet@hotmail.com>
2025-10-06 23:29:03 -0300

    adiciona classe Modificadores com comentários sobre modificadores de acesso

---
+++ src/Introducao/Modificadores/palavrasChaves.java
@@ -0,0 +1,8 @@
+package Introducao.modificadores;
+
+public class Modificadores {
+    // MODIFICADORES DE ACESSO
+     // public - Acesso de qualquer classe
+     // private - Acesso apenas dentro da classe
+     // protected - Acesso por classes no mesmo pacote e subclasses
+}
